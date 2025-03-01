7410dabd5dc19b1801a9e7ed57bbcb43b630bd14
Brenna Meyer <brennameyer9@gmail.com>
2025-03-01 16:11:15 -0500

    Add project files.

---
+++ Chapter 4 Exercise 10.cpp
@@ -0,0 +1,24 @@
+// Chapter 4 Exercise 10.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//Programmer:Brenna Meyer
+// Date: March 1st, 2025
+//Days in a Month
+//Write a program that asks the user to enter the month(letting the user enter an integer in the range of 1 through 12) and the year.
+// The program should then display the number of days in that month.Use the following criteria to identify leap years :
+
+//Determine whether the year is divisible by 100. If it is, then it is a leap year if and only if it is divisible by 400. 
+// For example, 2000 is a leap year but 2100 is not.
+//If the year is not divisible by 100, then it is a leap year if and only if it is divisible by 4. For example,
+// 2008 is a leap year but 2009 is not.
+
+//Here is a sample run of the program :
+//Enter a month(1–12) : 2 Enter
+//Enter a year : 2008 Enter
+//29 days
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
